90f365a4681524b3f1cd719a1cd2f1cd08d7fac9
Sohel Mia <sohelcse1999@gmail.com>
2020-04-04 22:58:17 +0600

    test setup

---
+++ tests/Feature/IntialsTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace Sohel\MarkdownParser\Test\Feature;
+
+
+
+use Orchestra\Testbench\TestCase;
+
+class IntialsTest extends TestCase
+{
+    public function test_true()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_false()
+    {
+        $this->assertFalse(false);
+    }
+}
